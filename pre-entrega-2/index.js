388d5921864185ddf3959ba0f9a7da39ed626aa8
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2023-12-20 14:35:09 -0700

    pre entrega 2 terminada

--- pre-entrega-2/index.js
+++ pre-entrega-2/index.js
@@ -1,38 +1,38 @@
-function ingresarPresionArterial(){
+function ingresarGlucosa() {
     //Definicion de la funcion
-    let registrosPresionArterial= [];
+    let registrosGlucosa = [];
     //Se crea un array paa almacenar los registros
     const MAXREGISTROS = 5;
     //Se establece un numero maximo de registros que se pueden ingresar
-    
-    for(let i = 1 ; i <= MAXREGISTROS ; i++){
+
+    for (let i = 1; i <= MAXREGISTROS; i++) {
         //Se crea un bucle para ingresar los datos
-        let sistolica=parseInt(prompt("Ingrese la presion arterial sitolica (registro " + i + "):"));
-        let diastolica= parseInt(prompt("Ingrese la presiom arterial diastolica (registro " + i + "):"));
+        let glucosa = parseInt(prompt("Ingrese tu cifra de glucosa (registro " + i + "):"));
         //Soliciar al usuario ingresar los datos
-        if(isNaN(sistolica) || isNaN(diastolica)){
+        if (glucosa > 70 || glucosa > 100) {
+            alert("Tu glucosa se encuentra elevada, favor de ir al medico")
+            //Se notifica al usuario si su nivel de glucosa se encuentra elevado.
+        } else if (isNaN(glucosa)) {
             alert("Por favor ingresa numeros validos");
             i--;
             //Se decremente para poder repetir la misma iteracion
             continue;
         }
         //Validacion de entrada y almacenamianto, para asegurar que los valores que se ingresen sen numero
-        let registro = {sistolica, 
-            diastolica};
-        registrosPresionArterial.push(registro)
+        let registro = { glucosa };
+        registrosGlucosa.push(registro)
         //si las entradas son validas se crea el objeto "registro" y se agrega al final del array
     }
-
-    console.log("Registros de presion arterial: ");
-    for(let i = 0 ; i < registrosPresionArterial.length ; i++){
-        let registro = registrosPresionArterial[i];
+    console.log("Registros de glucosa: ");
+    for (let i = 0; i < registrosGlucosa.length; i++) {
+        let registro = registrosGlucosa[i];
         console.log(
-            "Registro " + (i+1) + ": Sistolica = " + registro.sistolica + " , Diastolica = " + registro.diastolica
+            "Tu registro " + (i + 1) + ": de glucosa es = " + registro.glucosa + ", lo normal es entre 70 y 100 mg/dl"
         );
     }
     /*Mostrar los registros al usuario, se usa el bucle for para recorrer el array u mostrar cada registro,
-    tanto sistolico como diastolico
+    de la glucosa tomada por el prompt.
     */
 
 }
-ingresarPresionArterial()
+ingresarGlucosa()
